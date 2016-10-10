9b84a221928f982cd62f60059f378c51e1c1c8f5
David A. Lareo <dalareo@gmail.com>
2016-10-10 19:32:07 +0200

    error of syntax spelling getenv

--- docker-settings.py
+++ docker-settings.py
@@ -32,12 +32,12 @@ SECRET_KEY = os.getenv('TAIGA_SECRET_KEY')
 
 EMAIL_BACKEND = os.gentenv('EMAIL_BACKEND')
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
-EMAIL_HOST = os.gentenv('EMAIL_HOST')
-EMAIL_PORT = os.gentenv('EMAIL_PORT')
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.gentenv('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
-DEFAULT_FROM_EMAIL = os.gentenv('DEFAULT_FROM_EMAIL')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 
 if os.getenv('RABBIT_PORT') is not None and os.getenv('REDIS_PORT') is not None:
     from .celery import *
